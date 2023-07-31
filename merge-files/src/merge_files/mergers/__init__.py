832c2865af9225b6ede7197d6b389a8bce0a8342
Gareth Davidson <gaz@bitplane.net>
2023-07-31 15:01:46 +0100

    actually commit the code from the last update. facepalm.jpg

--- merge-files/src/merge_files/mergers/__init__.py
+++ merge-files/src/merge_files/mergers/__init__.py
@@ -1,4 +1,4 @@
-import os
+from pathlib import Path
 from typing import Callable
 
 from .concatenate import concatenate
@@ -12,8 +12,14 @@ def get(source_name: str, dest_name: str) -> Merger:
     Return the correct file merger
     """
 
-    source_type = os.path.splitext(source_name)[1]
-    dest_type = os.path.splitext(dest_name)[1]
+    source_path = Path(source_name)
+    dest_path = Path(dest_name)
+
+    source_ext = source_path.suffix or source_path.name
+    dest_ext = dest_path.suffix or dest_path.name
+
+    source_type = source_ext.lower()
+    dest_type = dest_ext.lower()
 
     if source_type == ".env" and dest_type == ".env":
         return env
