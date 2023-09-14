f191eabaca68525152c141dd4f6b2dd59584974d
Gareth Davidson <gaz@bitplane.net>
2023-09-14 08:47:05 +0100

    add test for equals in .env file

--- merge-files/tests/merge_files/test_env.py
+++ merge-files/tests/merge_files/test_env.py
@@ -10,72 +10,85 @@ def test_no_change():
 
 
 def test_overwrite_value():
-    dest = "\n".join(["KEY=custom"]).encode()
-    source = "\n".join(["KEY=default"]).encode()
+    target = "\n".join(["KEY=old_value"]).encode()
+    source = "\n".join(["KEY=new_value"]).encode()
 
-    expected = "\n".join(["KEY=default"]).encode()
+    expected = "\n".join(["KEY=new_value"]).encode()
 
-    actual = env(source, dest, True)
+    actual = env(source, target, True)
 
     assert actual == expected
 
 
-def test_change_variable():
-    dest = "\n".join(["KEY=mine", "HOST=localhost"]).encode()
+def test_add_variable_no_overwrite():
     source = "\n".join(["KEY=default", "NEW=cool"]).encode()
+    target = "\n".join(["KEY=mine", "HOST=localhost"]).encode()
 
     expected = "\n".join(["KEY=mine", "HOST=localhost", "NEW=cool"]).encode()
 
-    actual = env(source, dest)
+    actual = env(source, target)
+
+    assert actual == expected
+
+
+def test_variable_with_equals_sign():
+    target = "\n".join(["KEY=existing=overwrite", "NEW=new=value"]).encode()
+    source = "\n".join(["KEY=overwrite=me", "HOST=default=new"]).encode()
+
+    expected = "\n".join(
+        ["KEY=overwrite=me", "NEW=new=value", "HOST=default=new"]
+    ).encode()
+
+    actual = env(source, target, True)
 
     assert actual == expected
 
 
 def test_add_comments():
-    dest = "\n".join(["KEY=mine", "HOST=localhost"]).encode()
+    target = "\n".join(["KEY=mine", "HOST=localhost"]).encode()
     source = "\n".join(["KEY=default", "# Comment"]).encode()
 
     expected = "\n".join(["KEY=mine", "HOST=localhost", "# Comment"]).encode()
 
-    actual = env(source, dest)
+    actual = env(source, target)
 
     assert actual == expected
 
 
 def test_keep_comments():
-    dest = "\n".join(["KEY=custom", "# Comment"]).encode()
     source = "\n".join(["KEY=default", "OTHER=other_value # yup"]).encode()
+    target = "\n".join(["KEY=custom", "# Comment"]).encode()
 
     expected = "\n".join(
         ["KEY=custom", "# Comment", "OTHER=other_value # yup"]
     ).encode()
 
-    actual = env(source, dest)
+    actual = env(source, target)
 
     assert actual == expected
 
 
 def test_keep_comments_with_equals():
-    dest = "\n".join(["#UNCOMMENT_ME=n # explode()", "OTHER_VAR"]).encode()
     source = "\n".join(["NEW_VAR=Truthy"]).encode()
+    target = "\n".join(["#UNCOMMENT_ME=n # explode()", "OTHER_VAR"]).encode()
 
     expected = "\n".join(
         ["#UNCOMMENT_ME=n # explode()", "OTHER_VAR", "NEW_VAR=Truthy"]
     ).encode()
 
-    actual = env(source, dest)
+    actual = env(source, target)
 
     assert actual == expected
 
 
 def test_dont_duplicate_comments():
-    dest = "\n".join(["#UNCOMMENT_ME=n # explode()", "OTHER_VAR"]).encode()
     source = "\n".join(["#UNCOMMENT_ME=n # explode()", "NEW_VAR=y"]).encode()
+    target = "\n".join(["#UNCOMMENT_ME=n # explode()", "OTHER_VAR"]).encode()
 
     expected = "\n".join(
         ["#UNCOMMENT_ME=n # explode()", "OTHER_VAR", "NEW_VAR=y"]
     ).encode()
 
-    actual = env(source, dest)
+    actual = env(source, target)
 
     assert actual == expected
